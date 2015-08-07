eb8083b46a076023e709938c11de0fe2a4c48d59
killer@killer.com <killer@killer.com>
2015-08-07 10:44:12 +0800

    added comment

--- RSAConsoleApp/RSAConsoleApp/Program.cs
+++ RSAConsoleApp/RSAConsoleApp/Program.cs
@@ -15,15 +15,19 @@ namespace RSAConsoleApp
             using (var encrypterRSA = new RSACryptoServiceProvider())
             using (var decrypterRSA = new RSACryptoServiceProvider())
             {
+                // Write to string the Keys
                 var privXmlString = encrypterRSA.ToXmlString(true);
                 var publicXmlString = encrypterRSA.ToXmlString(false);
 
+                // Encrypt the message
                 var byteDataEncode = Encoding.UTF8.GetBytes("Message for you");
                 var encryptMsg = encrypterRSA.Encrypt(byteDataEncode, false);
 
+                // Decrypt
                 decrypterRSA.FromXmlString(privXmlString);
                 var decryptMsg = decrypterRSA.Decrypt(encryptMsg, false);
 
+                // Show the message
                 var originalMsg = Encoding.UTF8.GetString(decryptMsg);
                 Console.WriteLine(originalMsg);
             }
