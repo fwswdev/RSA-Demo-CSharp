214234b0d1aa41abdd8fc99f0c9bffef1c796e48
killer@killer.com <killer@killer.com>
2015-08-07 11:11:07 +0800

    added interface and header text for license and program description

--- RSAConsoleApp/RSAConsoleApp/Program.cs
+++ RSAConsoleApp/RSAConsoleApp/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+ * RSA Assymetric Demo by SevenString
+ * 
+ * The keys are stored either on memory or from file. 
+ * There is an interface to select between the two storage.
+ * 
+ * License: CC BY 2.0
+ * */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,8 +19,28 @@ namespace RSAConsoleApp
 {
     class Program
     {
+
+
+
+//-#-----#-----------------
+//-##---##---##---#-#----#-
+//-#-#-#-#--#--#--#-##---#-
+//-#--#--#-#----#-#-#-#--#-
+//-#-----#-######-#-#--#-#-
+//-#-----#-#----#-#-#---##-
+//-#-----#-#----#-#-#----#-
+//-------------------------
+
         static void Main()
         {
+            IAssymetricKeyStorage assymetricStorage;
+
+            // Comment either one of this lines to test for memory or xml storage
+            assymetricStorage = new AssymetricKeyStoreInMemory();
+            //assymetricStorage = new AssymetricKeyFromXmlFile("c:/privkey.xml", "c:/pubkey.xml");
+
+            Console.WriteLine(assymetricStorage.GetType().ToString()); // to see if we are storing it on memory or xml file
+
             using (var encrypterRSA = new RSACryptoServiceProvider())
             using (var decrypterRSA = new RSACryptoServiceProvider())
             {
@@ -19,12 +48,15 @@ namespace RSAConsoleApp
                 var privXmlString = encrypterRSA.ToXmlString(true);
                 var publicXmlString = encrypterRSA.ToXmlString(false);
 
+                assymetricStorage.StorePrivateKey(privXmlString);
+                assymetricStorage.StorePublicKey(publicXmlString);
+
                 // Encrypt the message
                 var byteDataEncode = Encoding.UTF8.GetBytes("Message for you");
                 var encryptMsg = encrypterRSA.Encrypt(byteDataEncode, false);
 
                 // Decrypt
-                decrypterRSA.FromXmlString(privXmlString);
+                decrypterRSA.FromXmlString(assymetricStorage.GetPrivateKey());
                 var decryptMsg = decrypterRSA.Decrypt(encryptMsg, false);
 
                 // Show the message
@@ -32,5 +64,106 @@ namespace RSAConsoleApp
                 Console.WriteLine(originalMsg);
             }
         }
+
+
+
+
+//-###--------------------------------------------------------
+//--#--#----#-#####-######-#####--######---##----####--######-
+//--#--##---#---#---#------#----#-#-------#--#--#----#-#------
+//--#--#-#--#---#---#####--#----#-#####--#----#-#------#####--
+//--#--#--#-#---#---#------#####--#------######-#------#------
+//--#--#---##---#---#------#---#--#------#----#-#----#-#------
+//-###-#----#---#---######-#----#-#------#----#--####--######-
+//------------------------------------------------------------
+
+
+
+        public interface IAssymetricKeyStorage
+        {
+            void StorePublicKey(string key);
+            void StorePrivateKey(string key);
+
+            string GetPrivateKey();
+        }
+
+
+
+
+//-#######-------------------------#-----#-----------------------------------
+//-#-------#####---####--#----#----##---##-######-#----#--####--#####--#---#-
+//-#-------#----#-#----#-##--##----#-#-#-#-#------##--##-#----#-#----#--#-#--
+//-#####---#----#-#----#-#-##-#----#--#--#-#####--#-##-#-#----#-#----#---#---
+//-#-------#####--#----#-#----#----#-----#-#------#----#-#----#-#####----#---
+//-#-------#---#--#----#-#----#----#-----#-#------#----#-#----#-#---#----#---
+//-#-------#----#--####--#----#----#-----#-######-#----#--####--#----#---#---
+//---------------------------------------------------------------------------
+
+
+        public class AssymetricKeyStoreInMemory : IAssymetricKeyStorage
+        {
+            string mPublicKey;
+            string mPrivateKey;
+
+            public void StorePublicKey(string key)
+            {
+                mPublicKey = key;
+            }
+
+            public void StorePrivateKey(string key)
+            {
+                mPrivateKey = key;
+            }
+
+            public string GetPrivateKey()
+            {
+                return mPrivateKey;
+            }
+        }
+
+
+
+//-#######-------------------------#######-----------------
+//-#-------#####---####--#----#----#-------#-#------######-
+//-#-------#----#-#----#-##--##----#-------#-#------#------
+//-#####---#----#-#----#-#-##-#----#####---#-#------#####--
+//-#-------#####--#----#-#----#----#-------#-#------#------
+//-#-------#---#--#----#-#----#----#-------#-#------#------
+//-#-------#----#--####--#----#----#-------#-######-######-
+//---------------------------------------------------------
+
+
+
+        public class AssymetricKeyFromXmlFile : IAssymetricKeyStorage
+        {
+            string mPrivKeyFile;
+            string mpublicKeyFile;
+
+
+            public AssymetricKeyFromXmlFile(string privKeyFile,string publicKeyFile)
+            {
+                mPrivKeyFile = privKeyFile;
+                mpublicKeyFile = publicKeyFile;
+            }
+
+
+            public void StorePublicKey(string key)
+            {
+                File.WriteAllText(mpublicKeyFile, key);
+            }
+
+            public void StorePrivateKey(string key)
+            {
+                File.WriteAllText(mPrivKeyFile, key);
+            }
+
+            public string GetPrivateKey()
+            {
+                return File.ReadAllText(mPrivKeyFile).Trim();
+            }
+        }
+
+
+
     }
 }
